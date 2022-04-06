6b69e24af69ce818d23f418ff2de5a6b198bf5ec
fcraveiro <dextruz@hotmail.com>
2022-04-05 22:07:14 -0300

    Pagina 1

--- lib/pagina1.dart
+++ lib/pagina1.dart
@@ -1,17 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
 
   @override
-  State<Login> createState() => _LoginState();
+  _LoginState createState() => _LoginState();
 }
 
 class _LoginState extends State<Login> {
+  final GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber,
+    GoogleSignInAccount? user = _googleSignIn.currentUser;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('aa'),
+        centerTitle: true,
+        actions: [
+          user == null
+              ? SizedBox(
+                  width: 30,
+                  height: 30,
+                  child: CircleAvatar(
+                    child: Image.asset(
+                      'assets/imagens/profile.png',
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                )
+              : SizedBox(
+                  width: 30,
+                  height: 30,
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      user.photoUrl.toString(),
+                      scale: 5,
+                    ),
+                  ),
+                ),
+          const SizedBox(
+            width: 15,
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 70,
+            ),
+            ElevatedButton(
+                child: const Text('Sign In'),
+                onPressed: user != null
+                    ? null
+                    : () async {
+                        await _googleSignIn.signIn();
+                        setState(() {});
+                      }),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                child: const Text('Sign Out'),
+                onPressed: user == null
+                    ? null
+                    : () async {
+                        await _googleSignIn.signOut();
+                        setState(() {});
+                      }),
+          ],
+        ),
+      ),
     );
   }
 }
